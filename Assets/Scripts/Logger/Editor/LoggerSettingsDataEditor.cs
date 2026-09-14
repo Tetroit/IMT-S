18d3a1c7d3ff42bd8b3fe2a2b2f9cd58cce75b6e
Tetroit <tetroit.git@gmail.com>
2026-09-14 12:01:01 +0200

    Logger script

---
+++ Assets/Scripts/Logger/Editor/LoggerSettingsDataEditor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Logger.Editor
+{
+    [CustomEditor(typeof(LoggerSettingsData))]
+    public class LoggerSettingsDataEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            LoggerSettingsData loggerSettingsData = (LoggerSettingsData)target;
+            base.OnInspectorGUI();
+            if (GUILayout.Button("Test"))
+            {
+                Logger.Test();
+            }
+        }
+    }
+}
